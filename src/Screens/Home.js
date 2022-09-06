4a41a34086d480a66b38b84d6a78687a0c719ea8
Udit Malhotra <udit.malhotra@spenda.co>
2022-09-06 07:19:25 +0530

    Web changes 1.0

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -2,12 +2,14 @@ import { React, useRef, useEffect, useState } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 import { gsap, Power3 } from 'gsap';
 import { Udit } from '../Assets/Udit';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import FaceIcon from '@mui/icons-material/Face';
+import IntegrationInstructionsIcon from '@mui/icons-material/IntegrationInstructions';
+import ArticleIcon from '@mui/icons-material/Article';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import DevicesIcon from '@mui/icons-material/Devices';
 import CancelIcon from '@mui/icons-material/Cancel';
-import SchoolIcon from '@mui/icons-material/School';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
 import HomeIcon from '@mui/icons-material/Home';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import './style.scss';
@@ -70,13 +72,11 @@ const Home = (props) => {
       {skills ?
         <IconButton className="absolute top-[4rem] md:top-0 left-[0] md:left-[1rem] self-start" onClick={() => { handleSkillsToggle(); }}><Tooltip title="Home" placement='left'><KeyboardBackspaceIcon fontSize='large' className='text-[#666] dark:text-lightgray cursor-pointer' sx={{ fontSize: '3rem' }} /></Tooltip></IconButton>
         : ''}
-      <div className='z-20 header nav-item absolute bg-slate dark:bg-black top-0 w-full h-16 flex flex-row basis-full justify-between items-center px-6 shadow-md shadow-lightgray dark:shadow-darkgray'>
-        <div className='flex flex-row justify-between items-center'>
-          <a href="https://medium.com/@uditmalhotraa" rel="noreferrer" target="_self" className=' font-normal tracking-wide nav-item-foot sm:my-0 text-black dark:text-white text-lg transition ease-in-out duration-700 hover:scale-105 select-none '><button className=' border px-3 py-1 font-light hover:text-[#00CCFF]'>Work</button></a>
-          <a href="https://drive.google.com/file/d/1vZLC6Jq4nVV-YMVfvSOY7jkug3w315A6/view?usp=sharing" rel="noreferrer" target="_self" className=' font-normal tracking-wide nav-item-foot mx-5 my-2 sm:my-0 text-black dark:text-white text-lg transition ease-in-out duration-700 hover:scale-105 select-none '><button className=' border px-3 py-1 font-light hover:text-[#FF3300]'>Resume</button></a>
-        </div>
+      <div className='z-20 header nav-item absolute bg-slate dark:bg-black top-0 w-full h-16 flex flex-row basis-full justify-between items-center px-6'>
+        <Tooltip title="Refesh" placement='bottom'><a href="/" className='text-black dark:text-white text-2xl tracking-wide font-normal'>Udit</a></Tooltip>
         <div className='flex flex-col border-2 border-black dark:border-white h-8 w-14 bg-transparent rounded-full relative justify-center cursor-pointer' onClick={() => { props.handleToggle(); setFlag(!flag) }}>
-          <div className={flag ? "flex flex-col bg-white h-5 w-5 rounded-full absolute right-0 mx-1" : "flex flex-col bg-black h-5 w-5 rounded-full absolute left-0 mx-1"}></div>
+          {/* <div className={flag ? "flex flex-col bg-white h-5 w-5 rounded-full absolute right-0 mx-1" : "flex flex-col bg-black h-5 w-5 rounded-full absolute left-0 mx-1"}></div> */}
+          {!flag ? <LightModeIcon className='absolute left-0 mx-1'/> : <DarkModeIcon className=' text-white absolute right-0 mx-1' />}
         </div>
       </div>
       {!skills ?
@@ -112,25 +112,24 @@ const Home = (props) => {
           </div>
         </div>
       }
-      <div ref={el => { bottomBar = el }} className='z-20 bg-slate dark:bg-black absolute bottom-0 w-full h-8 flex flex-row basis-full justify-between items-center px-6 shadow-boxy-light dark:shadow-boxy-dark'>
-        <p className='text-darkgray w-full text-center dark:text-lightgray text-xl tracking-wide font-normal'>uditmalhotra98@gmail.com</p>
+      <div ref={el => { bottomBar = el }} className='z-20 bg-[#36BADA] dark:bg-[#36BADA] absolute bottom-0 w-full h-8 flex flex-row basis-full justify-between items-center px-6 rounded-t-2xl'>
+        <Tooltip title="Email" placement='top'><a href='mailto:uditmalhotra98@gmail.com' target="_parent" rel="noopener noreferrer" className='text-white w-full text-center dark:text-black text-xl tracking-wide font-normal'>uditmalhotra98@gmail.com</a></Tooltip>
       </div>
 
       {show ?
         <div className='z-20 nav-item absolute right-[0.5rem] bottom-[2.5rem] flex flex-col justify-center items-center mr-1'>
           {!skills ?
-            <IconButton onClick={() => { handleSkillsToggle(); handleTogglee() }}><Tooltip title="Skills" placement='left'><SchoolIcon fontSize='large' className='text-[#666] dark:text-lightgray cursor-pointer' sx={{ fontSize: '3rem' }} /></Tooltip></IconButton>
+            <IconButton className='flex flex-col' onClick={() => { handleSkillsToggle(); handleTogglee() }}><Tooltip title="Skills" placement='left'><IntegrationInstructionsIcon fontSize='large' className='text-[#666] dark:text-lightgray cursor-pointer' sx={{ fontSize: '3rem' }} /></Tooltip><p className='text-black dark:text-white text-sm tracking-wide font-normal'>Skills</p></IconButton>
             :
             <IconButton onClick={() => { handleSkillsToggle(); handleTogglee() }}><Tooltip title="Home" placement='left'><HomeIcon fontSize='large' className='text-[#666] dark:text-lightgray cursor-pointer' sx={{ fontSize: '3rem' }} /></Tooltip></IconButton>
           }
-          <IconButton href="https://linkedin.com/in/uditmalhotraa" target="_self" rel="noopener noreferrer"><Tooltip title="Linkedin" placement='left'><LinkedInIcon fontSize='large' className='text-[#666] dark:text-lightgray cursor-pointer' sx={{ fontSize: '3rem' }} /></Tooltip></IconButton>
-          <IconButton href="https://instagram.com/uditmalhotraa" target="_self" rel="noopener noreferrer"><Tooltip title="Instagram" placement='left'><InstagramIcon fontSize='large' className='text-[#666] dark:text-lightgray cursor-pointer' sx={{ fontSize: '3rem' }} /></Tooltip></IconButton>
-          <IconButton href="https://open.spotify.com/user/8m59lr39cu8zunxmrd9c2uavb?si=64c34848a0954120" target="_self" rel="noopener noreferrer"><Tooltip title="Spotify" placement='left'><PlayCircleIcon fontSize='large' className='text-[#666] dark:text-lightgray cursor-pointer' sx={{ fontSize: '3rem' }} /></Tooltip></IconButton>
+          <IconButton className='flex flex-col' href="https://medium.com/@uditmalhotraa" target="_parent" rel="noopener noreferrer"><Tooltip title="Work" placement='left'><DevicesIcon fontSize='large' className='text-[#666] dark:text-lightgray cursor-pointer' sx={{ fontSize: '3rem' }} /></Tooltip><p className='text-black dark:text-white text-sm tracking-wide font-normal'>Work</p></IconButton>
+          <IconButton className=" flex flex-col" href="https://drive.google.com/file/d/1vZLC6Jq4nVV-YMVfvSOY7jkug3w315A6/view?usp=sharing" target="_parent" rel="noopener noreferrer"><Tooltip title="Resume" placement='left'><ArticleIcon fontSize='large' className='text-[#666] dark:text-lightgray cursor-pointer' sx={{ fontSize: '3rem' }} /></Tooltip><p className='text-black dark:text-white text-sm tracking-wide font-normal'>Resume</p></IconButton>
           <IconButton onClick={handleTogglee} ><CancelIcon fontSize='large' className='cursor-pointer' sx={{ color: '#DA5636', fontSize: '4rem', textShadow: "0px 0px 4px #666" }} /></IconButton>
         </div>
         :
         <div className='z-20 circle absolute right-[0.5rem] bottom-[2.5rem] flex flex-col justify-center items-center gap-y-2 mr-1'>
-          <IconButton onClick={handleTogglee} ><AddCircleIcon fontSize="large" className='cursor-pointer' sx={{ color: '#36BADA', fontSize: '4rem' }} /></IconButton>
+          <IconButton onClick={handleTogglee} className=" animate-bounce" ><AddCircleIcon fontSize="large" className='cursor-pointer' sx={{ color: '#36BADA', fontSize: '4rem' }} /></IconButton>
         </div>
       }
     </div>
